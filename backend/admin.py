be34ce1be2bacd640ef3d3a0f2818c37440d9a99
Talha Masood <talhamasood0000@gmail.com>
2025-03-20 14:02:33 +0500

    Add basic models and migrations

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Company, Order
 
-# Register your models here.
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number')
+    search_fields = ('name', 'phone_number')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('number', 'company', 'time', 'customer_name', 'customer_phone_number')
+    list_filter = ('company', 'time')
+    search_fields = ('number', 'customer_name', 'customer_phone_number')
+    ordering = ('-time',)
--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    number = models.CharField(max_length=100)
+    details = models.TextField()    
+    time = models.DateTimeField()
+    customer_name = models.CharField(max_length=100)
+    customer_phone_number = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return f"{self.number} - {self.company.name}"
